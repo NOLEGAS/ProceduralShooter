0924f949bc24ce244ec9519e797780f0ac7e8e29
NoeGrü <grunbaum.noel@gmail.com>
2025-04-29 11:06:15 +0200

    Removed unused boxcollision on anchors

--- Source/ProceduralShooter/Anchor.cpp
+++ Source/ProceduralShooter/Anchor.cpp
@@ -10,11 +10,6 @@ UAnchor::UAnchor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	NeighborDetector = CreateDefaultSubobject<UBoxComponent>(FName("NeighborDetector"));
-	NeighborDetector->AttachToComponent(this, FAttachmentTransformRules::KeepRelativeTransform);
-	NeighborDetector->SetBoxExtent(FVector(32.f, 32.f, 96.f));
-	NeighborDetector->SetRelativeLocation(FVector(50, 0, 0));
-	NeighborDetector->OnComponentBeginOverlap.AddDynamic(this, &UAnchor::DetectNeighbors);
 	// ...
 }
 
@@ -41,16 +36,4 @@ void UAnchor::CloseHole()
 {
 	GetWorld()->SpawnActor<AActor>(PlugActor, GetComponentLocation(), GetComponentRotation());
 	DestroyComponent();
-}
-
-void UAnchor::DetectNeighbors(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-    if (OtherActor && OtherActor->FindComponentByClass<UBoxComponent>() == OtherComp)
-    {
-        // Found another actor's BoxComponent
-    	if (GEngine)
-    	{
-    		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("[%s]Detected Neighbor! %s"), *GetOwner()->GetName(), *OtherActor->GetName()));
-    	}
-    }
 }
--- Source/ProceduralShooter/Anchor.h
+++ Source/ProceduralShooter/Anchor.h
@@ -27,10 +27,5 @@ public:
 	                           FActorComponentTickFunction* ThisTickFunction) override;
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<AActor> PlugActor;
-	UPROPERTY()
-	UBoxComponent* NeighborDetector;
 	void CloseHole();
-	UFUNCTION()
-	void DetectNeighbors(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	//void OnOverlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
