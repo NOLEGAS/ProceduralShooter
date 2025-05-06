6e6ac437639f54f48cfa1763806c15d20561bb1c
NoeGrü <grunbaum.noel@gmail.com>
2025-05-06 15:10:40 +0200

    Added visual bullet for player

--- Source/ProceduralShooter/Room.h
+++ Source/ProceduralShooter/Room.h
@@ -30,7 +30,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	UPROPERTY(EditAnywhere)
-	UBoxComponent* DetectionBox;
+	UBoxComponent* DetectionBox; //box component used for checking room collision against a single simple shape
 	UPROPERTY(VisibleAnywhere)
 	USceneComponent* Root;
 	UPROPERTY(EditAnywhere)
